397169f86e041affc67e528c277f64b35dcab618
nefelibetas <1241142935@qq.com>
2023-11-19 19:40:24 +0800

    实体类加上JDK8时间序列化和反序列化注释

--- src/main/java/com/fish/entity/pojo/Topic.java
+++ src/main/java/com/fish/entity/pojo/Topic.java
@@ -1,6 +1,10 @@
 package com.fish.entity.pojo;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.annotation.Table;
@@ -70,11 +74,15 @@ public class Topic implements Serializable {
     /**
      * 题目创建时间
      */
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createTime;
 
     /**
      * 题目更新时间
      */
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime updateTime;
 
     /**

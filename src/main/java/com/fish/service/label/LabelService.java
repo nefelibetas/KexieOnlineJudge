c608e036c2ce5bf6151c7c2cd40c70e8b6142206
nefelibetas <1241142935@qq.com>
2023-11-24 22:35:25 +0800

    添加JavaDoc，在实现类中更新标签完善了校验id

--- src/main/java/com/fish/service/label/LabelService.java
+++ src/main/java/com/fish/service/label/LabelService.java
@@ -7,10 +7,40 @@ import com.mybatisflex.core.service.IService;
 import java.util.ArrayList;
 
 public interface LabelService extends IService<Label> {
+    /**
+     * 新增id
+     * @param label 标签
+     * @return 封装好的响应信息
+     */
     Result<?> addLabel(Label label);
+    /**
+     * 批量增加标签
+     * @param labels 标签数组
+     * @return 封装好的响应信息
+     */
     Result<?> addLabelBatch(ArrayList<Label> labels);
+    /**
+     * 获取全部标签
+     * @return 封装好的响应信息
+     */
     Result<ArrayList<Label>> getLabels();
+    /**
+     * 获取id对应的标签
+     * @param labelId 标签id
+     * @return 封装好的响应信息
+     */
     Result<Label> getLabel(Long labelId);
+    /**
+     * 更新标签内容
+     * @param label 标签，需要校验标签对象内id是否为空
+     * @return 封装好的响应信息
+     */
     Result<?> updateLabel(Label label);
+    /**
+     * 真删除标签，需要root权限。<br/>
+     * 在删除前,需要清除其他表中的记录
+     * @param labelId 标签id
+     * @return 封装好的响应信息
+     */
     Result<?> deleteLabel(Long labelId);
 }

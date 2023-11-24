e49da4d0f67fb1b63930feb25bb2d4dbab04e339
nefelibetas <1241142935@qq.com>
2023-11-24 22:36:43 +0800

    添加JavaDoc

--- src/main/java/com/fish/controller/TopicController.java
+++ src/main/java/com/fish/controller/TopicController.java
@@ -20,44 +20,91 @@ public class TopicController {
     TopicService topicService;
     @Resource
     TopicLabelService topicLabelService;
+    /**
+     * 新增题目
+     * @param topic 题目
+     * @return 响应code为200表示成功
+     */
     @PostMapping("/admin/topic/add")
     public Result<?> addTopic(@Valid Topic topic) {
         return topicService.addTopic(topic);
     }
+    /**
+     * 批量增加题目
+     * @param topics 题目数组
+     * @return 响应code为200表示成功
+     */
     @PostMapping("/admin/topic/adds")
     public Result<?> addTopicBatch(@Valid ArrayList<Topic> topics) {
         return topicService.addTopicBatch(topics);
     }
+    /**
+     * 给题目批量绑定标签
+     * @param topicId 题目id
+     * @param labelIds 要加的所有标签id
+     * @return 响应code为200表示成功
+     */
     @PostMapping("/admin/topic/addLabel/{topicId}")
     public Result<?> addTopicLabel(
             @PathVariable("topicId") @NotNull(message = "题目id不能为空") Long topicId,
             @NotEmpty(message = "必须选择一个标签") @RequestBody ArrayList<Long> labelIds) {
         return topicLabelService.addLabelToTopic(topicId, labelIds);
     }
+    /**
+     * 获取该题目id可选的标签
+     * @param topicId 题目id
+     * @return 所有可选的标签
+     */
     @GetMapping("/admin/topic/getOptionalLabel/{topicId}")
     public Result<ArrayList<Label>> getOptionalLabels(@PathVariable("topicId") @NotNull(message = "题目id不能为空") Long topicId) {
         return topicLabelService.getOptionalLabels(topicId);
     }
-    @DeleteMapping("/admin/topic/delete/{topicId}")
+    /**
+     * 删除题目
+     * @param topicId 题目id
+     * @return 响应code为200表示成功
+     */
+    @PutMapping("/admin/topic/delete/{topicId}")
     public Result<?> deleteTopic(@PathVariable("topicId") @NotNull(message = "题目Id不能为空") Long topicId) {
         return topicService.deleteTopic(topicId);
     }
+    /**
+     * 删除该题目的一些标签
+     * @param topicId 题目id
+     * @param labelIds 一些标签的id
+     * @return 响应code为200表示成功
+     */
     @DeleteMapping("/admin/topic/deleteLabel/{topicId}")
     public Result<?> deleteTopicLabel(
             @PathVariable("topicId") @NotNull(message = "题目Id不能为空") Long topicId,
             @NotEmpty(message = "必须选择一个标签") @RequestBody ArrayList<Long> labelIds) {
         return topicLabelService.removeLabels(topicId, labelIds);
     }
+    /**
+     * 修改题目
+     * @param topicId 题目id
+     * @param topic 题目
+     * @return 响应code为200表示成功
+     */
     @PutMapping("/admin/topic/update/{topicId}")
     public Result<?> updateTopic(
             @PathVariable("topicId") @NotNull(message = "题目Id不能为空") Long topicId,
             @Valid @RequestBody Topic topic) {
         return topicService.updateTopic(topicId, topic);
     }
+    /**
+     * 获取所以题目
+     * @return 所有启用的题目
+     */
     @GetMapping("/topic/gets")
     public Result<ArrayList<TopicVO>> getTopics() {
         return topicService.getTopics();
     }
+    /**
+     * 获取对应id的题目
+     * @param topicId 题目id
+     * @return 该id对应的题目
+     */
     @GetMapping("/topic/get/{topicId}")
     public Result<TopicVO> getTopic(@PathVariable("topicId") @NotNull(message = "题目Id不能为空") Long topicId) {
         return topicService.getTopic(topicId);
--- src/main/java/com/fish/service/topic/TopicService.java
+++ src/main/java/com/fish/service/topic/TopicService.java
@@ -8,10 +8,40 @@ import com.mybatisflex.core.service.IService;
 import java.util.ArrayList;
 
 public interface TopicService extends IService<Topic> {
+    /**
+     * 新增题目
+     * @param topic 题目
+     * @return 封装好的响应信息
+     */
     Result<?> addTopic(Topic topic);
+    /**
+     * 批量新增题目
+     * @param topics 题目数组
+     * @return 封装好的响应信息
+     */
     Result<?> addTopicBatch(ArrayList<Topic> topics);
+    /**
+     * 删除对应题目
+     * @param topicId 题目id
+     * @return 封装好的响应信息
+     */
     Result<?> deleteTopic(Long topicId);
-    Result<?> updateTopic(Long topicId, Topic topicDTO);
+    /**
+     * 更新题目信息
+     * @param topicId 题目id
+     * @param topic 题目传输对象
+     * @return 封装好的响应信息
+     */
+    Result<?> updateTopic(Long topicId, Topic topic);
+    /**
+     * 获取所有题目
+     * @return 封装好的响应信息
+     */
     Result<ArrayList<TopicVO>> getTopics();
+    /**
+     * 获取id对应的题目
+     * @param topicId 题目id
+     * @return 封装好的响应信息
+     */
     Result<TopicVO> getTopic(Long topicId);
 }

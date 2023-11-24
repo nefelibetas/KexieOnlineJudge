ea5b11b80b6a1ce0d0efbbcc4586d127d2adf112
nefelibetas <1241142935@qq.com>
2023-11-24 22:35:25 +0800

    添加JavaDoc，在实现类中更新标签完善了校验id

--- src/main/java/com/fish/service/label/LabelServiceImpl.java
+++ src/main/java/com/fish/service/label/LabelServiceImpl.java
@@ -1,12 +1,13 @@
 package com.fish.service.label;
 
+import static com.fish.entity.pojo.table.LabelTableDef.LABEL;
 import com.fish.common.Result;
 import com.fish.entity.pojo.Label;
 import com.fish.exception.ServiceException;
 import com.fish.exception.ServiceExceptionEnum;
 import com.fish.mapper.LabelMapper;
-import com.fish.service.label.LabelService;
 import com.fish.utils.ResultUtil;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,7 +50,10 @@ public class LabelServiceImpl extends ServiceImpl<LabelMapper, Label> implements
     @Transactional
     @Override
     public Result<?> updateLabel(Label label) {
-        int i = mapper.update(label);
+        if (Objects.isNull(label.getLabelId()))
+            throw new ServiceException(ServiceExceptionEnum.KEY_ARGUMENT_NOT_INPUT);
+        QueryWrapper wrapper = QueryWrapper.create().select().from(LABEL).where(LABEL.LABEL_ID.eq(label.getLabelId()));
+        int i = mapper.updateByQuery(label, wrapper);
         if (i > 0)
             return ResultUtil.success();
         throw new ServiceException(ServiceExceptionEnum.OPERATE_ERROR);

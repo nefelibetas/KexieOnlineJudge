a056621d090e9049e0488dc0a6a3bf9ee8f772f9
nefelibetas <1241142935@qq.com>
2023-11-27 19:37:54 +0800

    迁移DTO、VO至kotlin 迁移Security相关至kotlin

---
+++ src/main/kotlin/com/fish/entity/dto/LoginAccountDTO.kt
@@ -0,0 +1,9 @@
+package com.fish.entity.dto
+
+import jakarta.validation.constraints.NotBlank
+import java.io.Serializable
+
+data class LoginAccountDTO(
+    var email: @NotBlank(message = "邮箱不能为空") String? = null,
+    var password: @NotBlank(message = "密码不能为空") String? = null
+) : Serializable
---
+++ src/main/kotlin/com/fish/security/UserDetailsServiceImpl.kt
@@ -0,0 +1,35 @@
+package com.fish.security
+
+import com.fish.exception.ServiceException
+import com.fish.exception.ServiceExceptionEnum
+import com.fish.mapper.AccountMapper
+import com.fish.mapper.RoleMapper
+import jakarta.annotation.Resource
+import lombok.extern.slf4j.Slf4j
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class UserDetailsServiceImpl : UserDetailsService {
+    @Resource
+    private val accountMapper: AccountMapper? = null
+    @Resource
+    private val roleMapper: RoleMapper? = null
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails {
+        val account = accountMapper!!.getAccountByEmail(username)
+        if (Objects.isNull(account))
+            throw ServiceException(ServiceExceptionEnum.ACCOUNT_NOT_FOUND)
+        return LoginAccount(account, getRoles(account.roleId))
+    }
+    private fun getRoles(roleId: Long?): ArrayList<SystemAuthority?> {
+        val role = roleMapper!!.getRoleById(roleId)
+        val simpleGrantedAuthority = SystemAuthority(role.roleName)
+        val authorities = ArrayList<SystemAuthority?>()
+        authorities.add(simpleGrantedAuthority)
+        return authorities
+    }
+}

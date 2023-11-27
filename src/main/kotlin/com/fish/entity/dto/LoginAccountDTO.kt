0dc6da001385e1878c820b711c104275ffa3606b
nefelibetas <1241142935@qq.com>
2023-11-27 20:39:28 +0800

    将LoginAccountDTO改为data class

--- src/main/kotlin/com/fish/entity/dto/LoginAccountDTO.kt
+++ src/main/kotlin/com/fish/entity/dto/LoginAccountDTO.kt
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank
 import java.io.Serializable
 
 data class LoginAccountDTO(
-    var email: @NotBlank(message = "邮箱不能为空") String?,
-    var password: @NotBlank(message = "密码不能为空") String?
+    val email: @NotBlank(message = "邮箱不能为空") String?,
+    val password: @NotBlank(message = "密码不能为空") String?
 ) : Serializable {
     constructor() : this(null, null)
 }

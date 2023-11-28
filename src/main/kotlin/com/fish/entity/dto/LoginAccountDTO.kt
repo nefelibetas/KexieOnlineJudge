a485d52e532412cf6067cef110ffd05e9c2fb075
BridgeFish <93062875+nefelibetas@users.noreply.github.com>
2023-11-28 15:37:54 +0800

    Revert "将LoginAccountDTO改为data class"
    This reverts commit 0dc6da001385e1878c820b711c104275ffa3606b.

--- src/main/kotlin/com/fish/entity/dto/LoginAccountDTO.kt
+++ src/main/kotlin/com/fish/entity/dto/LoginAccountDTO.kt
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank
 import java.io.Serializable
 
 data class LoginAccountDTO(
-    val email: @NotBlank(message = "邮箱不能为空") String?,
-    val password: @NotBlank(message = "密码不能为空") String?
+    var email: @NotBlank(message = "邮箱不能为空") String?,
+    var password: @NotBlank(message = "密码不能为空") String?
 ) : Serializable {
     constructor() : this(null, null)
 }

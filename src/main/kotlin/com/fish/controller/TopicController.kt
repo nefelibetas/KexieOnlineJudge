2f62a121b4dea1fdc68e0fd33955be6edf0c9288
BridgeFish <93062875+nefelibetas@users.noreply.github.com>
2023-11-28 15:37:54 +0800

    Revert "修复同LabelController中的错误"
    This reverts commit 500b9aff6c710eedd7d386104c009c84d356a1e8.

--- src/main/kotlin/com/fish/controller/TopicController.kt
+++ src/main/kotlin/com/fish/controller/TopicController.kt
@@ -102,14 +102,13 @@ class TopicController {
         return topicService!!.updateTopic(topicId, topic)
     }
 
-    /**
-     * 获取所以题目
-     * @return 所有启用的题目
-     */
-    @GetMapping("/topic/gets")
-    fun getTopics(): Result<ArrayList<TopicVO>> {
-        return topicService!!.getTopics()
-    }
+    @get:GetMapping("/topic/gets")
+    val topics: Result<ArrayList<TopicVO>>
+        /**
+         * 获取所以题目
+         * @return 所有启用的题目
+         */
+        get() = topicService!!.getTopics()
 
     /**
      * 获取对应id的题目

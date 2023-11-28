cec4361a3f3c63d52c60bb01d0e786a0a8a38f28
BridgeFish <93062875+nefelibetas@users.noreply.github.com>
2023-11-28 15:37:54 +0800

    Revert "增加了RegisterAccountDTO和LoginAccountDTO,用于接收来自注册、登陆请求接口的数据。 在controller、service中新增了javadoc，便于后人维护。"
    This reverts commit f683d865a8bd1486c4cff25e3e8fb9182e2373e3.

--- src/main/java/com/fish/mapper/AccountMapper.java
+++ src/main/java/com/fish/mapper/AccountMapper.java
@@ -1,9 +1,7 @@
 package com.fish.mapper;
 
-import com.fish.entity.dto.RegisterAccountDTO;
 import com.fish.entity.pojo.Account;
 import com.mybatisflex.core.BaseMapper;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -23,18 +21,10 @@ public interface AccountMapper extends BaseMapper<Account> {
     int deleteAccount(@Param("userId")String userId);
     @Update("update oj_account set role_id = #{roleId} where user_id = #{userId}")
     int changeAccountRole(@Param("userId")String userId, @Param("roleId")Long roleId);
-    @Select("select user_id, avatar, username, student_id, gender, specialty, nickname, email, qq, github_address, blog_address " +
-            "from oj_account " +
-            "where enabled = true and role_id < 3")
+    @Select("select * from oj_account where enabled = true and role_id < 3")
     ArrayList<Account> getAdmins();
-    @Select("select user_id, avatar, username, student_id, gender, specialty, nickname, email, qq, github_address, blog_address " +
-            "from oj_account " +
-            "where enabled = true and role_id = 3")
+    @Select("select * from oj_account where enabled = true and role_id = 3")
     ArrayList<Account> getCustomAccounts();
     @Select("select * from oj_account where enabled =true and user_id = #{userId}")
     Account getAccount(@Param("userId")String userId);
-    @Insert("insert into " +
-            "oj_account (nickname, avatar, student_id, username, password, gender, email, specialty, qq, blog_address, github_address) " +
-            "value (#{nickname}, #{avatar}, #{studentId}, #{username}, #{password}, #{gender}, #{email}, #{specialty}, #{qq}, #{blog_address}, #{github_address})")
-    int addAccount(RegisterAccountDTO registerAccountDTO);
 }
